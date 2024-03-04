96e234449e43bd09165bae11dc9df18623323d4a
Alex Mak <alexmak889@gmail.com>
2024-03-04 16:38:11 +0100

    soon workig

--- Computer_vision.py
+++ Computer_vision.py
@@ -3,13 +3,27 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 import cv2
+import numpy as np
 
-# initialize the camera and grab a reference to the raw camera capture
+def region_of_interest(edges):
+    height, width = edges.shape
+    mask = np.zeros_like(edges)
+
+    #Shows half of the screen
+    polygon = np.array([[
+        (0, height * 1 // 2),
+        (width, height * 1 // 2),
+        (width, height),
+        (0, height),
+    ]], np.int32)
+    cv2.fillPoly(mask, polygon, 255)
+    cropped_edges = cv2.bitwise_and(edges, mask)
+    return cropped_edges
+
 camera = PiCamera()
 camera.resolution = (640, 480)
 camera.framerate = 32
 rawCapture = PiRGBArray(camera, size=(640, 480))
-
 
 time.sleep(0.1)
 
@@ -19,7 +33,9 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     
     edges = cv2.Canny(gray, 100, 200)
-    cv2.imshow("Frame", edges)
+    
+    cropped_edges = region_of_interest(edges)
+    cv2.imshow("Frame", cropped_edges,)
     
     key = cv2.waitKey(1) & 0xFF
     
@@ -27,3 +43,5 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     
     if key == ord("q"):
         break
+
+cv2.destroyAllWindows()
