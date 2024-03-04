c80767081b02352ee2f49f32c573a537b5831f08
Alex Mak <alexmak889@gmail.com>
2024-03-04 15:04:57 +0100

    not working pid. The computer vision is now working with the camera.

--- Computer_vision.py
+++ Computer_vision.py
@@ -1,41 +1,29 @@
-
-# import the necessary packages
+# import the packages
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 import cv2
+
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
 camera.resolution = (640, 480)
 camera.framerate = 32
 rawCapture = PiRGBArray(camera, size=(640, 480))
-# allow the camera to warmup
+
+
 time.sleep(0.1)
-# capture frames from the camera
+
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-	# grab the raw NumPy array representing the image, then initialize the timestamp
-	# and occupied/unoccupied text
-	image = frame.array
-	# show the frame
-	cv2.imshow("Frame", image)
-	key = cv2.waitKey(1) & 0xFF
-	# clear the stream in preparation for the next frame
-	rawCapture.truncate(0)
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
-	
-
-#import cv2 as cv
-#import numpy as np
-
-#mg = cv.imread('photos/road1.png')
-#cv.imshow('orginal', img)
-
-#gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-#cv.imshow('Gray',gray)
-
-#canny = cv.Canny(gray, 240, 320)
-#cv.imshow('Canny', canny)
-#
-	#cv.waitKey(0)
+    image = frame.array
+    
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+    edges = cv2.Canny(gray, 100, 200)
+    cv2.imshow("Frame", edges)
+    
+    key = cv2.waitKey(1) & 0xFF
+    
+    rawCapture.truncate(0)
+    
+    if key == ord("q"):
+        break
