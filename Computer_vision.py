1b4d21d6b17ef39cefce3ed9f2769146202c1274
Alex Mak <alexmak889@gmail.com>
2024-03-08 14:43:33 +0100

    working but not verry well

--- Computer_vision.py
+++ Computer_vision.py
@@ -3,64 +3,137 @@ from picamera import PiCamera
 import time
 import cv2
 import numpy as np
+import logging
 
-
-def make_coordinates(image, line_parameters):
-    slope, intercept = line_parameters
-    y1 = image.shape[0]
-    y2 = int(y1*(3/5))
-    x1 = int((y1 - intercept)/slope)
-    x2 = int((y2 - intercept)/slope)
-    return np.array([x1, y1, x2, y2])
-
-    
-
-def display_lines(image, lines):
-    line_image = np.zeros_like(image)
-    if lines is not None:
-        for line in lines:
-            x1, y1, x2, y2 = line[0]
-            cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
-    return line_image
-    
+def detect_edges(frame):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    edges = cv2.Canny(blur, 100, 200)
+    return edges
 
 def region_of_interest(edges):
     height, width = edges.shape
     mask = np.zeros_like(edges)
-
-    # Shows half of the screen
+    
     polygon = np.array([[
         (0, height * 1 // 2),
         (width, height * 1 // 2),
         (width, height),
         (0, height),
     ]], np.int32)
+    
     cv2.fillPoly(mask, polygon, 255)
-    masked_image = cv2.bitwise_and(edges, mask)
-    return masked_image
+    cropped_edges = cv2.bitwise_and(edges, mask)
+    
+    return cropped_edges
+
+
+def detect_line_segments(cropped_edges):
+    # tuning min_threshold, minLineLength, maxLineGap is a trial and error process by hand
+    rho = 1  # distance precision in pixel, i.e. 1 pixel
+    angle = np.pi / 180  # angular precision in radian, i.e. 1 degree
+    min_threshold = 10  # minimal of votes
+    line_segments = cv2.HoughLinesP(cropped_edges, rho, angle, min_threshold, np.array([]), minLineLength=8, maxLineGap=4)
+
+    return line_segments
+
+
+def average_slope_intercept(frame, line_segments):
+    """
+    This function combines line segments into one or two lane lines
+    If all line slopes are < 0: then we only have detected left lane
+    If all line slopes are > 0: then we only have detected right lane
+    """
+    lane_lines = []
+    if line_segments is None:
+        logging.info('No line_segment segments detected')
+        return lane_lines
+    height, width, _ = frame.shape
+    left_fit = []
+    right_fit = []
+
+    boundary = 1/3
+    left_region_boundary = width * (1 - boundary)  # left lane line segment should be on left 2/3 of the screen
+    right_region_boundary = width * boundary # right lane line segment should be on left 2/3 of the screen
+
+    for line_segment in line_segments:
+        for x1, y1, x2, y2 in line_segment:
+            if x1 == x2:
+                logging.info('skipping vertical line segment (slope=inf): %s' % line_segment)
+                continue
+            fit = np.polyfit((x1, x2), (y1, y2), 1)
+            slope = fit[0]
+            intercept = fit[1]
+            if slope < 0:
+                if x1 < left_region_boundary and x2 < left_region_boundary:
+                    left_fit.append((slope, intercept))
+            else:
+                if x1 > right_region_boundary and x2 > right_region_boundary:
+                    right_fit.append((slope, intercept))
+
+    left_fit_average = np.average(left_fit, axis=0)
+    if len(left_fit) > 0:
+        lane_lines.append(make_points(frame, left_fit_average))
+
+    right_fit_average = np.average(right_fit, axis=0)
+    if len(right_fit) > 0:
+        lane_lines.append(make_points(frame, right_fit_average))
+
+    logging.debug('lane lines: %s' % lane_lines)  # [[[316, 720, 484, 432]], [[1009, 720, 718, 432]]]
+
+    return lane_lines
+
+def make_points(frame, line):
+    height, width, _ = frame.shape
+    slope, intercept = line
+    y1 = height  # bottom of the frame
+    y2 = int(y1 * 1 / 2)  # make points from middle of the frame down
+
+    # bound the coordinates within the frame
+    x1 = max(-width, min(2 * width, int((y1 - intercept) / slope)))
+    x2 = max(-width, min(2 * width, int((y2 - intercept) / slope)))
+    return [[x1, y1, x2, y2]]
+
+def detect_lane(frame):
+    
+    edges = detect_edges(frame)
+    cropped_edges = region_of_interest(edges)
+    line_segments = detect_line_segments(cropped_edges)
+    lane_lines = average_slope_intercept(frame, line_segments)
+    
+    return lane_lines
+
+def display_lines(frame, lines, line_color=(0, 255, 0), line_width=2):
+    line_image = np.zeros_like(frame)
+    if lines is not None:
+        for line in lines:
+            for x1, y1, x2, y2 in line:
+                cv2.line(line_image, (x1, y1), (x2, y2), line_color, line_width)
+    line_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
+    return line_image
+
+
 
 
 camera = PiCamera()
 camera.resolution = (640, 480)
 camera.framerate = 32
+
 rawCapture = PiRGBArray(camera, size=(640, 480))
-
 time.sleep(0.1)
+
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-    image = frame.array
-    lane_image = np.copy(image) 
-
-    gray = cv2.cvtColor(lane_image, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray,(5, 5),0)
-    canny = cv2.Canny(blur, 100, 200)
-
-
-    cropped_image = region_of_interest(canny)
-    lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
-    line_image = display_lines(lane_image, lines)
-    combine_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
-
-    cv2.imshow("Frame", combine_image)
+    if frame is None:
+        break
+    
+    edges = detect_edges(frame.array)
+    cropped_edges = region_of_interest(edges)
+    line_segments = detect_line_segments(cropped_edges)
+    lane_lines = average_slope_intercept(frame.array, line_segments)
+    
+    lane_lines_image = display_lines(frame.array, lane_lines)
+    
+    cv2.imshow("lane lines", lane_lines_image)
     
     key = cv2.waitKey(1) & 0xFF
     
